bb9236b84fa9ca371fb7ba813ca7382c9c8505d1
Preva1l <prod.preva1l@gmail.com>
2025-03-19 22:43:04 +1100

    refactor: remove duplicate code

--- Bukkit/src/main/java/info/preva1l/fadah/data/dao/sqlite/ListingSQLiteDao.java
+++ Bukkit/src/main/java/info/preva1l/fadah/data/dao/sqlite/ListingSQLiteDao.java
@@ -4,13 +4,10 @@ import com.google.common.collect.Lists;
 import com.zaxxer.hikari.HikariDataSource;
 import info.preva1l.fadah.Fadah;
 import info.preva1l.fadah.data.dao.Dao;
-import info.preva1l.fadah.records.listing.BidListing;
-import info.preva1l.fadah.records.listing.BinListing;
+import info.preva1l.fadah.data.dao.common_sql.CommonSQLDao;
 import info.preva1l.fadah.records.listing.Listing;
-import info.preva1l.fadah.utils.ItemSerializer;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang.NotImplementedException;
-import org.bukkit.inventory.ItemStack;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -18,7 +15,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
-import java.util.TreeSet;
 import java.util.UUID;
 import java.util.logging.Level;
 
@@ -42,52 +38,9 @@ public class ListingSQLiteDao implements Dao<Listing> {
                         FROM `listings`
                         WHERE `uuid`=?;""")) {
                 statement.setString(1, id.toString());
-                final ResultSet resultSet = statement.executeQuery();
+                ResultSet resultSet = statement.executeQuery();
                 if (resultSet.next()) {
-                    final UUID ownerUUID = UUID.fromString(resultSet.getString("ownerUUID"));
-                    final String ownerName = resultSet.getString("ownerName");
-                    String temp = resultSet.getString("category");
-                    String currency;
-                    String categoryID;
-                    if (temp.contains("~")) {
-                        String[] t2 = temp.split("~");
-                        currency = t2[1];
-                        categoryID = t2[0];
-                    } else {
-                        currency = "vault";
-                        categoryID = temp;
-                    }
-                    final long creationDate = resultSet.getLong("creationDate");
-                    final long deletionDate = resultSet.getLong("deletionDate");
-                    final double price = resultSet.getDouble("price");
-                    final double tax = resultSet.getDouble("tax");
-                    final ItemStack itemStack = ItemSerializer.deserialize(resultSet.getString("itemStack"))[0];
-                    final boolean biddable = resultSet.getBoolean("biddable");
-
-                    final Listing listing;
-                    if (biddable) {
-                        listing = new BidListing(
-                                id,
-                                ownerUUID, ownerName,
-                                itemStack,
-                                categoryID,
-                                currency, price, tax,
-                                creationDate, deletionDate,
-                                new TreeSet<>()
-                        );
-                    } else {
-                        listing = new BinListing(
-                                id,
-                                ownerUUID, ownerName,
-                                itemStack,
-                                categoryID,
-                                currency, price, tax,
-                                creationDate, deletionDate,
-                                new TreeSet<>()
-                        );
-                    }
-
-                    return Optional.of(listing);
+                    return Optional.ofNullable(CommonSQLDao.create(id, resultSet));
                 }
             }
         } catch (SQLException e) {
@@ -113,50 +66,7 @@ public class ListingSQLiteDao implements Dao<Listing> {
                 final ResultSet resultSet = statement.executeQuery();
                 while (resultSet.next()) {
                     final UUID id = UUID.fromString(resultSet.getString("uuid"));
-                    final UUID ownerUUID = UUID.fromString(resultSet.getString("ownerUUID"));
-                    final String ownerName = resultSet.getString("ownerName");
-                    String temp = resultSet.getString("category");
-                    String currency;
-                    String categoryID;
-                    if (temp.contains("~")) {
-                        String[] t2 = temp.split("~");
-                        currency = t2[1];
-                        categoryID = t2[0];
-                    } else {
-                        currency = "vault";
-                        categoryID = temp;
-                    }
-                    final long creationDate = resultSet.getLong("creationDate");
-                    final long deletionDate = resultSet.getLong("deletionDate");
-                    final double price = resultSet.getDouble("price");
-                    final double tax = resultSet.getDouble("tax");
-                    final ItemStack itemStack = ItemSerializer.deserialize(resultSet.getString("itemStack"))[0];
-                    final boolean biddable = resultSet.getBoolean("biddable");
-
-                    final Listing listing;
-                    if (biddable) {
-                        listing = new BidListing(
-                                id,
-                                ownerUUID, ownerName,
-                                itemStack,
-                                categoryID,
-                                currency, price, tax,
-                                creationDate, deletionDate,
-                                new TreeSet<>()
-                        );
-                    } else {
-                        listing = new BinListing(
-                                id,
-                                ownerUUID, ownerName,
-                                itemStack,
-                                categoryID,
-                                currency, price, tax,
-                                creationDate, deletionDate,
-                                new TreeSet<>()
-                        );
-                    }
-
-                    retrievedData.add(listing);
+                    retrievedData.add(CommonSQLDao.create(id, resultSet));
                 }
                 return retrievedData;
             }
@@ -180,18 +90,7 @@ public class ListingSQLiteDao implements Dao<Listing> {
                          `creationDate`, `deletionDate`, `price`, `tax`,
                          `itemStack`, `biddable`, `bids`)
                         VALUES (?,?,?,?,?,?,?,?,?,?,?);""")) {
-                statement.setString(1, listing.getId().toString());
-                statement.setString(2, listing.getOwner().toString());
-                statement.setString(3, listing.getOwnerName());
-                statement.setString(4, listing.getCategoryID() + "~" + listing.getCurrencyId());
-                statement.setLong(5, listing.getCreationDate());
-                statement.setLong(6, listing.getDeletionDate());
-                statement.setDouble(7, listing.getPrice());
-                statement.setDouble(8, listing.getTax());
-                statement.setString(9, ItemSerializer.serialize(listing.getItemStack()));
-                statement.setBoolean(10, false);
-                statement.setString(11, "");
-                statement.execute();
+                CommonSQLDao.buildStatement(listing, statement).execute();
             }
         } catch (SQLException e) {
             Fadah.getConsole().log(Level.SEVERE, "Failed to add item to listings!", e);
